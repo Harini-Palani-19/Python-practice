cc232eb1746dc391e7cc543a42dd2dea753e1769
Harini Palani <harinipalani52@gmail.com>
2026-01-29 01:04:57 +0530

    Add shape identification based on user input

---
+++ shape.py
@@ -0,0 +1,11 @@
+n=int(input("Enter a number:"))
+if(n==1):
+  print("Circle")
+elif(n==2):
+  print("Rectangle")
+elif(n==3):
+  print("Triangle")
+elif(n==4):
+  print("Square")
+else:
+  print("Other shapes")
